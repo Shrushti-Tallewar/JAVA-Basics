f5ac1178c5eb9b0fda35a2a195bf4eda42ff3735
Shrushti-Tallewar <tallewarshrushti5@gmail.com>
2026-07-12 21:46:54 +0530

    practise

---
+++ encapsulation1.java
@@ -0,0 +1,32 @@
+package project;
+class Student {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class encapsulation1 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Aman");
+        s.setAge(20);
+
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
+    }
+}
